c26f0d4013037838edcc7e2a6323af86c6408b46
sunshibao <664588619@qq.com>
2017-05-10 11:27:22 +0800

    init

---
+++ Jerry/script.js
@@ -0,0 +1,54 @@
+var getRandomColor = function(){ 
+	return "#"+("00000"+((Math.random()*16777215+0.5)>>0).toString(16)).slice(-6);
+};
+$(document).ready(function(){
+	//gridster
+	$(function(){ //DOM Ready
+	    $(".gridster ul").gridster({
+	        widget_margins: [10, 10],
+	        widget_base_dimensions: [140, 140]
+	    });
+	});
+
+	//hover
+	$(".gridster li").hover(function(){
+		$(this).find(".mask").addClass('animated bounceOutLeft');
+	},function(){
+		$(this).find(".mask").removeClass('animated bounceOutLeft');
+	});
+
+	//slide
+	var slideImgWidth = $(".slide img").eq(0).width();
+	var slideImgLength = $(".slide img").size();
+	var slideWidth = (slideImgWidth * slideImgLength)+"px";
+	var slideIndex = 0;
+	$(".slide").css({"width":slideWidth});
+	setInterval(function(){
+		if(slideIndex>=slideImgLength){
+			slideIndex = 0;
+		}
+		var left = -slideImgWidth*slideIndex
+		$(".slide").animate({"left":left},"slow","swing");
+		var info = $(".slide span").eq(slideIndex).html();
+		$(".slide_info p").html(info);
+		slideIndex++;
+	},4000);
+
+	//bg 
+	var r = Math.ceil(Math.random() *10 );
+	console.log(r);
+	$("body").css("background","url(images/b/"+r+".jpg) no-repeat center");
+
+	//title
+	var iTitles = new Array('不求超越别人，但求超越自己','最足以显示一个人性格的，莫过于他所嘲笑的是什么东西','Tomorrow is another day! ', '有人就有恩怨，有恩怨就有江湖。人就是江湖，你怎么退出？', '做人如果没有梦想，那跟咸鱼有什么区别？','求知若渴，虚心若愚','往往都是事情改变人，人却改变不了事情','你想成为哪一类人，并不取决于你的能力，而是取决于你的选择','强者自救,圣者渡人','生活就像一盒巧克力，你永远不知道你会得到什么。','恐惧让你沦为囚犯，希望让你重获自由','当你不能够再拥有，你唯一可以做的，就是令自己不要忘记。','如果你有梦想，就要守护它。','世界上再没有地方像家一样。','为什么总是正义战胜邪恶？因为只有赢的一方才代表正义'); iTitlesIndex = Math.floor(Math.random() * iTitles.length); document.title = '孙世宝的主页 | '+ iTitles[iTitlesIndex];
+
+	//fancybox
+	$(document).ready(function() {
+		//$(".fancybox").fancybox({type: 'iframe'});
+	});
+
+});
+window.onload = function(){
+	$(".spinner").hide();
+	$(".gridster").show();
+}
---
+++ readme.md
@@ -0,0 +1,80 @@
+## 联系方式
+
+- QQ：664588619
+- Email：664588619@qq.com 
+- 微信号：baobaog521
+
+---
+
+## 个人信息
+
+ * 孙世宝/男/1991 
+ * 山东职业技术学院/计算机
+ * 工作年限：3年
+ * 英语：Cet-4
+ * 技术博客：http://skill86.lofter.com
+ * Github: https://github.com/jerryaimee
+ * 职位：PHP工程师，前端工程师，项目经理。
+ * 城市：上海
+
+---
+
+## 工作经历
+
+### 寻钱网 (2014年6月 ~ 2015年6月)
+负责寻钱网网站及手机app整体架构设计及其他相关代码编写。
+
+### 温州中括号科技【红石金融集团】 (2012年6月 ~ 2014年6月)
+任网络部技术负责人，主要负责公司互联网产品开发、技术架构。
+
+ 
+### 其他 (2011年2月 ~ 2012年2月)
+先后就职于温州航天信息计算机有限公司负责项目开发及维护。
+
+
+---
+
+## 作品及案例
+
+### 寻钱网 （平台+app）
+寻钱网为温州中祯资产管理有限公司旗下的独立金融品牌，是提供金融资产互联网理财的网络信息服务平台，月资金交易额100万以上。
+是温州本土首家获千万元风投的股票投融资平台。
+在公司期间负责网站平台的核心架构及主要代码的编写、iOS手机App开发。
+网址 ： http://www.xunqianwang.com
+
+### 手机App
+先后为温州新派服饰集团开发iPad版内部资料浏览IOS App、上海真帮商城手机版Android/ios App等。
+主要基于phonegap+HTML5技术混合开发：
+客户端使用Angular.js框架作为核心库，通过JSON同服务端进行数据交互，服务端采用PHP+Mysql开发，基于Laravel框架。
+负责该项目的核心架构及主要代码的编写。
+
+### 微信公众平台管理系统
+该项目为公司自主研发的微信管理平台，类似微盟。
+除了具有微信公众平台原有的功能，还开发了微信墙、微网站、微喜帖等功能，并为客户定制属于自己的微信公众系统。
+系统采用PHP+Mysql开发，本人负责该项目主要代码的编写。
+
+### 其他项目
+绿购商城、消防预警系统、ecshop项目开发、discuz项目二次开发、PHPCMS项目二次开发等。
+
+
+
+## 技能清单
+
+
+以下均为我熟练使用的技能
+
+* 语言：PHP/Javascript/Node/swift/C/iOS
+* Web框架：Lavaral/Express/Koa
+* 前端框架：Bootstrap/AngularJS/Jquery/EasyUi/Snap.svg/ionic
+* 前端工具：Bower/Gulp/PhoneGap
+* 数据库相关：MySQL/MongoDB/Redis/SQLite
+* 版本管理、文档和自动化部署工具：Svn/Git/Composer/NPM
+* 单元测试：PHPUnit/Jasmine
+* 操作系统：Linux/Mac OS/Centos
+
+
+---
+
+##
+* [返回主页](http://jerryaimee.github.io/ "Title")
+* power by markdown.
